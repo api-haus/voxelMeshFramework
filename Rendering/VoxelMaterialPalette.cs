36bcd332186ac582d28fc9d095d44f8e7ee2c0f7
Kassinity <yuri@api.haus>
2025-09-20 19:58:18 +0300

    fix: remove unused adobe reference

--- Rendering/VoxelMaterialPalette.cs
+++ Rendering/VoxelMaterialPalette.cs
@@ -1,16 +1,10 @@
 namespace Rendering
 {
-	using Adobe.Substance;
 	using UnityEngine;
 
 	[CreateAssetMenu(menuName = "🧊Voxel Meshing/Material Palette 🎨")]
 	public class VoxelMaterialPalette : ScriptableObject
 	{
-#if ADOBE_SUBSTANCE3D
-		[SerializeField]
-		SubstanceGraphSO[] substanceGraphs;
-#endif
-
 		[SerializeField]
 		int resolution = 1024;
 	}
